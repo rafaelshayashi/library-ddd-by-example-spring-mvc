2d73f87b8fbb3aaa0acc3535745296d25fd60c34
Rafael Simionato Hayashi <rafaelshayashi@gmail.com>
2021-02-27 12:32:04 -0300

    :white_check_mark: add test already existing book

--- catalogue/src/test/java/br/com/rafaelshayashi/catalogue/service/BookServiceTest.java
+++ catalogue/src/test/java/br/com/rafaelshayashi/catalogue/service/BookServiceTest.java
@@ -5,6 +5,7 @@ import br.com.rafaelshayashi.catalogue.controller.request.BookValueRequest;
 import br.com.rafaelshayashi.catalogue.model.Book;
 import br.com.rafaelshayashi.catalogue.model.BookValue;
 import br.com.rafaelshayashi.catalogue.repository.BookRepository;
+import br.com.rafaelshayashi.catalogue.util.exception.ResourceAlreadyExistsException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,11 +33,30 @@ public class BookServiceTest {
     @Test
     @DisplayName("Service - Should create a book")
     public void should_create_a_book() {
-        BookValue bookValue = BookValue.builder().currency("BRL").amount(6200).build();
-        Book bookMock = Book.builder().title("Effective Java").isbn("978-0134685991").value(bookValue).build();
-        doReturn(Optional.empty()).when(repository).findByIsbn(any(String.class));
-        doReturn(bookMock).when(repository).save(any(Book.class));
 
+        doReturn(Optional.empty()).when(repository).findByIsbn(any(String.class));
+        doReturn(getBookMock()).when(repository).save(any(Book.class));
+
+        Book book = service.create(getBookRequest());
+
+        Assertions.assertEquals("Effective Java", book.getTitle());
+        Assertions.assertEquals("978-0134685991", book.getIsbn());
+    }
+
+    @Test
+    @DisplayName("Service - try to create a already existing book")
+    public void try_to_create_a_already_existing_book() {
+
+        doReturn(Optional.of(getBookMock())).when(repository).findByIsbn(any(String.class));
+        Assertions.assertThrows(ResourceAlreadyExistsException.class, () -> service.create(getBookRequest()));
+    }
+
+    private Book getBookMock() {
+        BookValue bookValue = BookValue.builder().currency("BRL").amount(6200).build();
+        return Book.builder().title("Effective Java").isbn("978-0134685991").value(bookValue).build();
+    }
+
+    private BookRequest getBookRequest() {
         BookValueRequest bookValueRequest = new BookValueRequest();
         bookValueRequest.setCurrency("BRL");
         bookValueRequest.setAmount(6200);
@@ -44,10 +64,6 @@ public class BookServiceTest {
         request.setTitle("Effective Java");
         request.setIsbn("978-0134685991");
         request.setValue(bookValueRequest);
-        Book book = service.create(request);
-
-        Assertions.assertEquals("Effective Java", book.getTitle());
-        Assertions.assertEquals("978-0134685991", book.getIsbn());
-
+        return request;
     }
 }
