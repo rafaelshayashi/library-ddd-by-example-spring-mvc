8624852e9fad76055c7ae444308599c9bde59cb7
Rafael Simionato Hayashi <rafaelshayashi@gmail.com>
2021-02-28 11:21:06 -0300

    :white_check_mark: add test for find library service

--- library/src/test/java/br/com/rafaelshayashi/library/service/LibraryBranchServiceTest.java
+++ library/src/test/java/br/com/rafaelshayashi/library/service/LibraryBranchServiceTest.java
@@ -18,6 +18,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = LibraryServiceImpl.class)
@@ -32,28 +33,49 @@ class LibraryBranchServiceTest {
     @Test
     @DisplayName("Should create a library")
     void should_create_a_library() {
-        Address addressMock = Address.builder().street("Rua da Consolação, 94").state("São Paulo").country("Brasil").zipCode("01302-000").build();
-        LibraryBranch libraryMock = LibraryBranch.builder().name("Biblioteca mario de andrade").address(addressMock).build();
+        // getting mock objects and mock requests
+        LibraryBranch libraryMock = getLibraryBranchMock();
+        LibraryBranchRequest libraryRequest = getLibraryBranchRequest();
 
+        // Setting up mockito
         Mockito.doReturn(Optional.empty()).when(repository).findByName(ArgumentMatchers.any(String.class));
         Mockito.doReturn(libraryMock).when(repository).save(ArgumentMatchers.any(LibraryBranch.class));
 
-        LibraryBranchRequest libraryRequest = new LibraryBranchRequest();
-        libraryRequest.setName("Biblioteca mario de andrade");
-        LibraryAddressRequest libraryAddressRequest = new LibraryAddressRequest();
-        libraryAddressRequest.setStreet("Rua da Consolação, 94");
-        libraryAddressRequest.setState("São Paulo");
-        libraryAddressRequest.setCountry("Brasil");
-        libraryAddressRequest.setZipCode("01302-000");
-        libraryRequest.setAddress(libraryAddressRequest);
+        // Calling the service
         LibraryBranch library = service.create(libraryRequest);
 
+        // Assertions
         Assertions.assertEquals("Biblioteca mario de andrade", library.getName());
     }
 
     @Test
     @DisplayName("Try to create a already existing library")
     void try_to_create_a_already_existing_library() {
+        // getting mock objects and mock requests
+        LibraryBranchRequest libraryRequest = getLibraryBranchRequest();
+        LibraryBranch libraryMock = getLibraryBranchMock();
+
+        // Setting up mockito
+        Mockito.doReturn(Optional.of(libraryMock)).when(repository).findByName(ArgumentMatchers.any(String.class));
+
+        // Assertions
+        Assertions.assertThrows(ResourceAlreadyExistsException.class, () -> service.create(libraryRequest));
+    }
+
+    @Test
+    @DisplayName("Service - Should find a library by uuid")
+    void should_find_a_library_by_uuid() {
+        LibraryBranch libraryBranchMock = getLibraryBranchMock();
+
+        Mockito.doReturn(Optional.of(libraryBranchMock)).when(repository).findByUuid(ArgumentMatchers.any(UUID.class));
+
+        Optional<LibraryBranch> libraryBranch = service.find(UUID.randomUUID());
+
+        Assertions.assertTrue(libraryBranch.isPresent());
+        Assertions.assertEquals(libraryBranchMock.getName(), libraryBranch.get().getName());
+    }
+
+    private LibraryBranchRequest getLibraryBranchRequest() {
         LibraryBranchRequest libraryRequest = new LibraryBranchRequest();
         libraryRequest.setName("Biblioteca mario de andrade");
         LibraryAddressRequest libraryAddressRequest = new LibraryAddressRequest();
@@ -62,11 +84,20 @@ class LibraryBranchServiceTest {
         libraryAddressRequest.setCountry("Brasil");
         libraryAddressRequest.setZipCode("01302-000");
         libraryRequest.setAddress(libraryAddressRequest);
+        return libraryRequest;
+    }
 
-        Address addressMock = Address.builder().street("Rua da Consolação, 94").state("São Paulo").country("Brasil").zipCode("01302-000").build();
-        LibraryBranch libraryMock = LibraryBranch.builder().name("Biblioteca mario de andrade").address(addressMock).build();
-        Mockito.doReturn(Optional.of(libraryMock)).when(repository).findByName(ArgumentMatchers.any(String.class));
+    private LibraryBranch getLibraryBranchMock() {
+        Address addressMock = Address.builder()
+                .street("Rua da Consolação, 94")
+                .state("São Paulo")
+                .country("Brasil")
+                .zipCode("01302-000")
+                .build();
 
-        Assertions.assertThrows(ResourceAlreadyExistsException.class, () -> service.create(libraryRequest));
+        return LibraryBranch.builder()
+                .name("Biblioteca mario de andrade")
+                .address(addressMock)
+                .build();
     }
 }
