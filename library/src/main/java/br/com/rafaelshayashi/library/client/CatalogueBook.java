6f1b3b0efe86e6fc8c97fd8b1c49d8a97c37c474
Rafael Simionato Hayashi <rafaelshayashi@gmail.com>
2021-03-06 23:38:10 -0300

    :sparkles: add feign support

---
+++ library/src/main/java/br/com/rafaelshayashi/library/client/CatalogueBook.java
@@ -0,0 +1,16 @@
+package br.com.rafaelshayashi.library.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@FeignClient(value = "catalogue", url = "http://localhost:8080")
+public interface CatalogueBook {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/books/{uuid}")
+    Optional<BookResponse> detailsBook(@PathVariable UUID uuid);
+}
